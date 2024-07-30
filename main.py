60d947414655153d28c68dae0cd59fa2435a2312
awfulreternal <matveykudelin7@gmail.com>
2024-07-31 01:07:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,78 @@
-from telegram.ext import Updater
-from handlers import start, move, explore, inventory, craft, button
+import telebot
+from config import TOKEN
+from game import Game
+from messages import MESSAGES
 
-def main():
-    # Замените 'YOUR_TOKEN' на ваш токен, полученный от BotFather
-    TOKEN = '7013013514:AAG_KcuXDjDBjsRhT65hJE8mzOc9CdpnfCc'
-    updater = Updater(TOKEN)
-    dp = updater.dispatcher
+bot = telebot.TeleBot(TOKEN)
+game = Game()
 
-    dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(CommandHandler('move', move))
-    dp.add_handler(CommandHandler('explore', explore))
-    dp.add_handler(CommandHandler('inventory', inventory))
-    dp.add_handler(CommandHandler('craft', craft))
-    dp.add_handler(CallbackQueryHandler(button))
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, MESSAGES['welcome'])
 
-    updater.start_polling()
-    updater.idle()
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, MESSAGES['help'])
+
+@bot.message_handler(commands=['status'])
+def status(message):
+    user_id = message.from_user.id
+    city_info = game.get_city_info(user_id)
+    bot.send_photo(message.chat.id, city_info['image'])
+    bot.reply_to(message, city_info['status'])
+
+@bot.message_handler(commands=['create'])
+def create_civilization(message):
+    user_id = message.from_user.id
+    success, msg = game.create_civilization(user_id)
+    bot.reply_to(message, msg)
+
+@bot.message_handler(commands=['upgrade'])
+def upgrade_building(message):
+    user_id = message.from_user.id
+    params = message.text.split(maxsplit=1)
+    if len(params) > 1:
+        building = params[1]
+        success, msg = game.upgrade_building(user_id, building)
+    else:
+        msg = MESSAGES['missing_params']
+    bot.reply_to(message, msg)
+
+@bot.message_handler(commands=['attack'])
+def attack(message):
+    user_id = message.from_user.id
+    params = message.text.split(maxsplit=1)
+    if len(params) > 1:
+        target_civilization = params[1]
+        success, msg = game.attack_city(user_id, target_civilization)
+    else:
+        msg = MESSAGES['missing_params']
+    bot.reply_to(message, msg)
+
+@bot.message_handler(commands=['quest'])
+def complete_quest(message):
+    user_id = message.from_user.id
+    params = message.text.split(maxsplit=1)
+    if len(params) > 1:
+        quest_type = params[1]
+        success, msg = game.complete_quest(user_id, quest_type)
+    else:
+        msg = MESSAGES['missing_params']
+    bot.reply_to(message, msg)
+
+@bot.message_handler(commands=['donate'])
+def donate(message):
+    user_id = message.from_user.id
+    params = message.text.split(maxsplit=1)
+    if len(params) > 1:
+        try:
+            donate_amount = int(params[1])
+            success, msg = game.donate(user_id, donate_amount)
+        except ValueError:
+            msg = MESSAGES['invalid_amount']
+    else:
+        msg = MESSAGES['missing_params']
+    bot.reply_to(message, msg)
 
 if __name__ == '__main__':
-    main()
+    bot.polling()
