a6f7f01bcd88a473990191c2460d0e47744127ec
awfulreternal <matveykudelin7@gmail.com>
2024-07-31 01:08:58 +0300

    Update and rename handlers.py to config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+
+CITY_IMAGES = {
+    'small_town': 'path_to_images/small_town.jpg',
+    'large_city': 'path_to_images/large_city.jpg',
+    'capital': 'path_to_images/capital.jpg',
+    'default': 'path_to_images/default.jpg'
+}
