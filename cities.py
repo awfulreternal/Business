aab9cc437e7b1e107938d821d268fff3fcdfbea4
awfulreternal <matveykudelin7@gmail.com>
2024-07-31 01:10:12 +0300

    Create cities.py

---
+++ cities.py
@@ -0,0 +1,6 @@
+CITY_IMAGES = {
+    'small_town': 'path_to_images/small_town.jpg',
+    'large_city': 'path_to_images/large_city.jpg',
+    'capital': 'path_to_images/capital.jpg',
+    'default': 'path_to_images/default.jpg'
+}
