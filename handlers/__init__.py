494bb84d299b46d9bd003e716e8167d1a5f73704
awfulreternal <matveykudelin7@gmail.com>
2024-07-30 09:49:12 +0300

    Delete handlers directory

--- handlers/__init__.py
+++
@@ -1,8 +0,0 @@
-from .start_handler import register_start_handler
-from .property_handler import register_property_handler
-from .business_handler import register_business_handler
-
-def register_handlers(bot):
-    register_start_handler(bot)
-    register_property_handler(bot)
-    register_business_handler(bot)
--- handlers/start_handler.py
+++
@@ -1,7 +0,0 @@
-from telebot import TeleBot
-from business_logic import start_game
-
-def register_start_handler(bot: TeleBot):
-    @bot.message_handler(commands=['start'])
-    def cmd_start(message):
-        start_game(bot, message)
